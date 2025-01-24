b13d75edb0b2b7cccaef9093e8329d59db692285
Victor <victor.meyo@student.moringaschool.com>
2025-01-24 04:26:51 +0300

    Final submission

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+//Remove 'main' node
+
+const main = document.querySelector('main');
+
+main.remove();
+
+//Create h1 node(element) and assign newHeader variable
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+//Assign newHeader your name
+
+newHeader.textContent = 'Victor is the champion'; 
+
+document.body.appendChild(newHeader);
